3978c0d3445299f486556ba84bdead81c98f2b9e
Boris <91095836+BorisKorsun@users.noreply.github.com>
2023-09-09 19:12:29 +0200

    adds modal-root (func. createPortal)

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import { Overlay, ModalContainer } from './Modal.styled';
+import { createPortal } from 'react-dom';
+
+const modalRoot = document.querySelector('#modal-root');
 
 class Modal extends Component {
   render() {
-    return (
+    return createPortal(
       <Overlay>
         <ModalContainer>
           <img src="" alt="" />
         </ModalContainer>
-      </Overlay>
+      </Overlay>,
+      modalRoot
     );
   }
 }
